fe636dafff84019ab207cc94ad96727668e9f6e7
von MUTINDA <maxwellwambua@gmail.com>
2020-06-25 09:51:11 +0300

    Update generate_qr.py

--- APIs/generate_qr.py
+++ APIs/generate_qr.py
@@ -24,12 +24,12 @@ def generate_code(user_data):
 
 if __name__ == '__main__':
 
-    name = 'von MUKANYE'
+    name = 'John Doe'
 
     # The data that you want to store
     user_data = {
         'name': name,
-        'mobile': 729309658,
+        'mobile': ,
         'country': 'KE',
         'image': ''
     }
