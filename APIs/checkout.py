e8377847175d101a0f904ef588582208d61813b5
von MUTINDA <maxwellwambua@gmail.com>
2020-06-25 09:49:21 +0300

    Update checkout.py

--- APIs/checkout.py
+++ APIs/checkout.py
@@ -19,7 +19,7 @@ def mpesa_checkout():
         # Initiate the checkout. If successful, you will get back a transactionId
         # transactionId = africastalking.PaymentService.mobile_checkout(productName,phoneNumber,currencyCode,amount,metadata)
         res = payment.mobile_checkout(
-            product_name='testProduct', phone_number='+254729309658', currency_code='KES', amount=10, metadata={
+            product_name='testProduct', phone_number='+', currency_code='KES', amount=10, metadata={
                 "agentId": "654",
                 "productId": "3366"
             })
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 
     # Specify the name of your Africa's Talking payment product
     productName = "testProduct"
-    phoneNumber =  '0729309658'
+    phoneNumber =  ''
     currencyCode = "KES"
     amount = 10.50
     metadata = {
